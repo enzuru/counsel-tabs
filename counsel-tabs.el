27a9ebd9bea88de15887efc83047a4b0ae884c91
Ahmed Khanzada <me@enzu.ru>
2023-07-15 17:15:34 -0400

    Move another function inside

--- counsel-tabs.el
+++ counsel-tabs.el
@@ -3,6 +3,11 @@
 ;; Copyright (C) enzu.ru
 ;; SPDX-License-Identifier: GPL-3.0
 
+(defun counsel-tabs-index-by-name (name &optional tabs frame)
+  ;; Return the index of TAB by the its NAME.
+  (seq-position (or tabs (funcall tab-bar-tabs-function frame))
+                name (lambda (a b) (equal (alist-get 'name a) b))))
+
 (defun counsel-tabs-recent-tabs (&optional tabs frame)
   (let* ((tabs (or tabs (funcall tab-bar-tabs-function frame))))
     (seq-sort-by (lambda (tab) (alist-get 'time tab)) #'>
@@ -22,7 +27,7 @@
             :history 'counsel-describe-symbol-history
             :require-match t
             :action (lambda (name)
-                      (tab-bar-select-tab (1+ (or (tab-bar--tab-index-by-name name) 0))))
+                      (tab-bar-select-tab (1+ (or (counsel-tabs-index-by-name name) 0))))
             :caller 'counsel-describe-tab))
 
 (provide 'counsel-tabs)
